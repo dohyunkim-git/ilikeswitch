bdb4d94fa382956409e14a2e3d88e209d5a4cc8c
Dohyun Kim <dohyunkim-git@users.noreply.github.com>
2020-05-06 23:40:05 +0900

    초기 커밋.

---
+++ ilikeswitch/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ilikeswitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("학년을 입력하세요 : ");
+            int level = int.Parse(Console.ReadLine());
+            switch(level)
+            {
+                case 1:
+                    Console.WriteLine("수강해야하는 전공 학점 : 12학점");
+                    break;
+                case 2:
+                    Console.WriteLine("수강해야하는 전공 학점 : 18학점");
+                    break;
+                case 3:
+                    Console.WriteLine("수강해야하는 전공 학점 : 10학점");
+                    break;
+                case 4:
+                    Console.WriteLine("수강해야하는 전공 학점 : 18학점");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
